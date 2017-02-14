c9c2e09182a28cd45d40537130f9b44cd82b5712
Craig Edwards <craig@blackdogfoundry.com>
2017-02-14 16:33:09 +1100

    Initial Commit

---
+++ FUKit/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FUKit
+//
+//  Created by Craig Edwards on 14/02/2017.
+//  Copyright © 2017 BlackDog Foundry Pty Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
