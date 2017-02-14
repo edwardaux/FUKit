6a80dbc8aab7624df6b59e8943c1d67e5022bb1d
Craig Edwards <craig@blackdogfoundry.com>
2017-02-14 16:47:57 +1100

    Added view demo

--- FUKit/main.swift
+++ FUKit/main.swift
@@ -8,13 +8,21 @@
 
 import Foundation
 
-class MyWindow : FUWindow {
-	override func touchesBegan(touches: [FUTouch]) -> Bool {
-		print(touches)
-		return true
-	}
-}
+let window1 = FUWindow(frame: NSRect(x: 10, y: 10, width: 150, height: 150))
+let view1 = FUView(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
+view1.backgroundColor = .cyan
+window1.contentView.addSubview(view: view1)
+
+let window2 = FUWindow(frame: NSRect(x: 210, y: 210, width: 150, height: 150))
+let view2 = FUView(frame: NSRect(x: 80, y: 80, width: 50, height: 50))
+view2.backgroundColor = .magenta
+let view3 = FUView(frame: NSRect(x: 5, y: 5, width: 10, height: 10))
+view3.backgroundColor = .black
+view2.addSubview(view: view3)
+window2.contentView.addSubview(view: view2)
+
 let app = FUApplication.shared
-app.addWindow(window: MyWindow(frame: NSRect(x: 10, y: 10, width: 150, height: 150)))
+app.addWindow(window: window1)
+app.addWindow(window: window2)
 app.run()
 
